09308dc182405622dc1ebef455bbafa18ad99ea7
13bscsmattiq <13bscsmattiq@seecs.edu.pk>
2016-04-15 23:27:06 +0500

    Create UnitTest.py

---
+++ UnitTest.py
@@ -0,0 +1,68 @@
+print"TEST:"
+if(GreedyCoinChange([1, 5, 10, 25],  30) == dynamicCoinChange( [1, 5, 10, 25],  30) ):
+    print "Same Result";
+else:
+    print"Different results:"
+    print"Greedy:", GreedyCoinChange([1, 5, 10, 25],  30)
+    print"Dynamic:", dynamicCoinChange( [1, 5, 10, 25],  30)
+if(GreedyCoinChange([1, 5, 10, 25],  100)== dynamicCoinChange( [1, 5, 10, 25],  100 )):
+    print "Same Result";
+else:
+    print"Different results:"
+    print"Greedy:", GreedyCoinChange([1, 5, 10, 25],  100)
+    print"Dynamic:", dynamicCoinChange( [1, 5, 10, 25],  100)
+    
+if(GreedyCoinChange([1, 5, 10, 25],  220), dynamicCoinChange( [1, 5, 10, 25],  220 )):
+    print "Same Result";
+else:
+    print"Different results:"
+    print"Greedy:", GreedyCoinChange([1, 5, 10, 25],  220)
+    print"Dynamic:", dynamicCoinChange( [1, 5, 10, 25],  220)
+    
+if(GreedyCoinChange([1, 5, 10, 25],  300), dynamicCoinChange( [1, 5, 10, 25],  300 )):
+    print "Same Result";
+else:
+    print"Different results:"
+    print"Greedy:", GreedyCoinChange([1, 5, 10, 25],  300)
+    print"Dynamic:", dynamicCoinChange( [1, 5, 10, 25],  300)
+    
+if(GreedyCoinChange([1, 5, 10, 25],  460), dynamicCoinChange( [1, 5, 10, 25],  460 )):
+    print "Same Result";
+else:
+    print"Different results:"
+    print"Greedy:", GreedyCoinChange([1, 5, 10, 25],  460)
+    print"Dynamic:", dynamicCoinChange( [1, 5, 10, 25],  460)
+    
+if(GreedyCoinChange([1, 5, 10, 25],  550), dynamicCoinChange( [1, 5, 10, 25],  550 )):
+    print "Same Result"
+else:
+    print"Different results:"
+    print"Greedy:", GreedyCoinChange([1, 5, 10, 25],  550)
+    print"Dynamic:", dynamicCoinChange( [1, 5, 10, 25],  550)
+    
+if(GreedyCoinChange([1, 5, 10, 25],  600), dynamicCoinChange( [1, 5, 10, 25],  600)):
+    print "Same Result";
+else:
+    print"Different results:"
+    print"Greedy:", GreedyCoinChange([1, 5, 10, 25],  600)
+    print"Dynamic:", dynamicCoinChange( [1, 5, 10, 25],  600)
+if(GreedyCoinChange([1, 5, 10, 25],  720), dynamicCoinChange( [1, 5, 10, 25],  720 )):
+    print "Same Result";
+else:
+    print"Different results:"
+    print"Greedy:", GreedyCoinChange([1, 5, 10, 25],  720)
+    print"Dynamic:", dynamicCoinChange( [1, 5, 10, 25],  720)
+    
+if(GreedyCoinChange([1, 5, 10, 25],  740), dynamicCoinChange( [1, 5, 10, 25],  740 )):
+    print "Same Result";
+else:
+    print"Different results:"
+    print"Greedy:", GreedyCoinChange([1, 5, 10, 25],  740)
+    print"Dynamic:", dynamicCoinChange( [1, 5, 10, 25],  740)
+    
+if(GreedyCoinChange([1, 5, 10, 25],  800), dynamicCoinChange( [1, 5, 10, 25],  800)):
+    print "Same Result";
+else:
+    print"Different results:"
+    print"Greedy:", GreedyCoinChange([1, 5, 10, 25],  800)
+    print"Dynamic:", dynamicCoinChange( [1, 5, 10, 25],  800)
